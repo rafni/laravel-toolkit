9b3fd8359e002cc2f85e729277f00c489578f9aa
Rafni <alberto.rcdb@gmail.com>
2017-08-03 12:49:00 +0200

    Now all replacements getters are in the trait

--- src/Console/Scaffolding/Traits/SharedMethods.php
+++ src/Console/Scaffolding/Traits/SharedMethods.php
@@ -54,4 +54,64 @@ trait SharedMethods
         return str_replace($keys, $values, $this->stub);
     }
     
+    /**
+     * Gets the namespace for this module
+     * 
+     * @return string
+     */
+    protected function getModule()
+    {
+        return studly_case($this->getPlural($this->argument('name')));
+    }
+    
+    /**
+     * Gets the name used by the model
+     * 
+     * @return string
+     */
+    protected function getModel()
+    {
+        return studly_case($this->getSingular($this->argument('name')));
+    }
+    
+    /**
+     * Gets the name used by the service
+     * 
+     * @return string
+     */
+    protected function getService()
+    {
+        return studly_case($this->getPlural($this->argument('name'))).'Service';
+    }
+    
+    /**
+     * Gets the name used by the contract
+     * 
+     * @return string
+     */
+    protected function getContract()
+    {
+        return studly_case($this->getPlural($this->argument('name'))).'Contract';
+    }
+    
+    /**
+     * Gets the name used by the controller
+     * 
+     * @return string
+     */
+    protected function getController()
+    {
+        return studly_case($this->getPlural($this->argument('name'))).'Controller';
+    }
+    
+    /**
+     * Gets the name of the table that the model uses
+     * 
+     * @return string
+     */
+    protected function getTableName()
+    {
+        return $this->getPlural($this->argument('name'));
+    }
+    
 }
